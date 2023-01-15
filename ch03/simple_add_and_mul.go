ae2b602d9be006a3f547e3065fb2eb8b45ccf2a5
Yeom-Yeom <diting9813@skuniv.ac.kr>
2023-01-15 19:30:04 +0900

    23.01.15

---
+++ ch03/simple_add_and_mul.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2, num3, result int
+	fmt.Scanln(&num1, &num2, &num3)
+	result = num1*num2 + num3
+	fmt.Printf("%d X %d + %d = %d", num1, num2, num3, result)
+}
